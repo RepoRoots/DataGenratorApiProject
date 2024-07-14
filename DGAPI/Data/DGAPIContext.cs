3c7bb4de7c91852b7c09b72b9f7067c9378b3926
RepoRoots <168888659+RepoRoots@users.noreply.github.com>
2024-07-14 12:04:48 -0400

    Api for DataGeneratorProject

---
+++ DGAPI/Data/DGAPIContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DbGenratorWithBogus.DbModels;
+
+    public class DGAPIContext : DbContext
+    {
+        public DGAPIContext (DbContextOptions<DGAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = default!;
+
+        public DbSet<Order> Orders { get; set; } = default!;
+
+        public DbSet<OrderDetail> OrderDetails { get; set; } = default!;
+
+    public DbSet<Product> Products { get; set; } = default!;
+    }
---
+++ DGAPI/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DGAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DGAPIContext") ?? throw new InvalidOperationException("Connection string 'DGAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
